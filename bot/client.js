fd555c58983ba7c9868e84439aeade0cf8c26cb0
David Constantin <constantin.david.andrei21@gmail.com>
2026-09-21 10:59:16 +0300

    Use path.resolve for paths

--- bot/client.js
+++ bot/client.js
@@ -26,7 +26,7 @@ class BotClient extends Client {
   }
 
   async loadCommands() {
-    const commandsPath = path.join(__dirname, 'commands');
+    const commandsPath = path.resolve(__dirname, 'commands');
     const commandFolders = fs.readdirSync(commandsPath);
 
     for (const folder of commandFolders) {
@@ -49,7 +49,7 @@ class BotClient extends Client {
   }
 
   async loadEvents() {
-    const eventsPath = path.join(__dirname, 'events');
+    const eventsPath = path.resolve(__dirname, 'events');
     const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
     for (const file of eventFiles) {
